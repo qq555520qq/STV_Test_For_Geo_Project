de9b3fa4b1c0def6433c2f4463f38f67a26bf664
Gene <qq555520qq@gmail.com>
2020-05-12 20:11:13 +0800

    Add Lab2.

--- src/test/java/com/github/davidmoten/geo/Base32Test.java
+++ src/test/java/com/github/davidmoten/geo/Base32Test.java
@@ -17,20 +17,50 @@ public class Base32Test {
     }
 
     @Test
-    public void encodeBase32WithOnlyNum() throws Exception {
-        String encode = Base32.encodeBase32(9223372036854775807L);
-        assertEquals("7zzzzzzzzzzzz", encode);
-    }
-
-    @Test
     public void encodeBase32WithPositiveNumAndBiggerLength() throws Exception {
         String encode = Base32.encodeBase32(9223372036854775807L, 14);
         assertEquals("07zzzzzzzzzzzz", encode);
     }
 
     @Test
-    public void encodeBase32WithNegativeNumAndNormalLength() throws Exception {
+    public void encodeBase32WithPositiveNumAndSmallerLength() throws Exception {
+        String encode = Base32.encodeBase32(9223372036854775807L, 13);
+        assertEquals("7zzzzzzzzzzzz", encode);
+    }
+
+    @Test
+    public void encodeBase32WithPositiveNumAndNegativeLength() throws Exception {
+        String encode = Base32.encodeBase32(9223372036854775807L, -2);
+        assertEquals("7zzzzzzzzzzzz", encode);
+    }
+
+    @Test
+    public void encodeBase32WithNegativeNumAndBiggerLength() throws Exception {
+        String encode = Base32.encodeBase32(-9223372036854775808L, 15);
+        assertEquals("-008000000000000", encode);
+    }
+
+    @Test
+    public void encodeBase32WithNegativeNumAndSmallerLength() throws Exception {
         String encode = Base32.encodeBase32(-9223372036854775808L, 9);
+        assertEquals("-8000000000000", encode);
+    }
+
+    @Test
+    public void encodeBase32WithNegativeNumAndNegativeLength() throws Exception {
+        String encode = Base32.encodeBase32(-9223372036854775808L, -2);
+        assertEquals("-8000000000000", encode);
+    }
+
+    @Test
+    public void encodeBase32WithOnlyPositiveLong() throws Exception {
+        String encode = Base32.encodeBase32(9223372036854775807L);
+        assertEquals("7zzzzzzzzzzzz", encode);
+    }
+
+    @Test
+    public void encodeBase32WithOnlyNegativeLong() throws Exception {
+        String encode = Base32.encodeBase32(-9223372036854775808L);
         assertEquals("-8000000000000", encode);
     }
 
--- src/test/java/com/github/davidmoten/geo/GeoHashTest.java
+++ src/test/java/com/github/davidmoten/geo/GeoHashTest.java
@@ -1,9 +1,12 @@
 package com.github.davidmoten.geo;
 
+import com.google.common.collect.Sets;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import com.github.davidmoten.geo.GeoHash.*;
+
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
@@ -18,11 +21,6 @@ public class GeoHashTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void fromLongToStringWithInvalidLong() {
-        GeoHash.fromLongToString(0);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
     public void adjacentHashWithNullHash() {
         GeoHash.adjacentHash(null, Direction.RIGHT);
     }
@@ -33,10 +31,221 @@ public class GeoHashTest {
     }
 
     @Test
-    public void testAdjacentWithDifferentDirection() {
+    public void testAdjacentWithDifferentDirectionAndSingularHash() {
         assertEquals("stt", GeoHash.adjacentHash("STV", Direction.BOTTOM));
         assertEquals("gzz", GeoHash.adjacentHash("STV", Direction.TOP));
         assertEquals("stu", GeoHash.adjacentHash("STV", Direction.LEFT));
         assertEquals("bpb", GeoHash.adjacentHash("STV", Direction.RIGHT));
+    }
+
+    @Test
+    public void testAdjacentWithDifferentDirectionAndEvenHash() {
+        assertEquals("stvu", GeoHash.adjacentHash("STVV", Direction.BOTTOM));
+        assertEquals("stvvz8", GeoHash.adjacentHash("STVVxx", Direction.TOP));
+        assertEquals("stvt", GeoHash.adjacentHash("STVV", Direction.LEFT));
+        assertEquals("bpbp", GeoHash.adjacentHash("STVV", Direction.RIGHT));
+    }
+
+    @Test
+    public void testAdjacentHashWithPositiveSteps() {
+        assertEquals("stvb", GeoHash.adjacentHash("STVV", Direction.BOTTOM, 5));
+        assertEquals("stvvzs", GeoHash.adjacentHash("STVVxx", Direction.TOP, 5));
+        assertEquals("stut", GeoHash.adjacentHash("STVV", Direction.LEFT, 5));
+        assertEquals("bpcp", GeoHash.adjacentHash("STVV", Direction.RIGHT, 5));
+    }
+
+    @Test
+    public void testAdjacentHashWithNegativeSteps() {
+        assertEquals("swjf", GeoHash.adjacentHash("STVV", Direction.BOTTOM, -5));
+        assertEquals("stvvxd", GeoHash.adjacentHash("STVVxx", Direction.TOP, -5));
+        assertEquals("bpcp", GeoHash.adjacentHash("STVV", Direction.LEFT, -5));
+        assertEquals("stut", GeoHash.adjacentHash("STVV", Direction.RIGHT, -5));
+    }
+
+    @Test
+    public void neighboursWithNegativeHash() {
+        Set<String> neighbours = Sets.newHashSet("-1", "-3", "-4", "-d", "-5", "-e", "-7", "-9");
+        assertEquals(neighbours, Sets.newHashSet(GeoHash.neighbours("-6")));
+    }
+
+    @Test
+    public void neighboursWithPositiveHash() {
+        Set<String> neighbours = Sets.newHashSet("stv49", "stufz", "stv4b", "stv42", "stufr", "stufx", "stv4c", "stv43");
+        assertEquals(neighbours, Sets.newHashSet(GeoHash.neighbours("STV48")));
+    }
+
+    @Test(expected = StringIndexOutOfBoundsException.class)
+    public void neighboursWithNegativeSpecialHash() {
+        GeoHash.neighbours("-$#");
+    }
+
+    @Test(expected = StringIndexOutOfBoundsException.class)
+    public void neighboursWithPositiveSpecialHash() {
+        GeoHash.neighbours("@#$");
+    }
+
+    @Test
+    public void hashLengthToCoverBoundingBoxWithSixteenBoundary() {
+        assertEquals(12, GeoHash.hashLengthToCoverBoundingBox(1, 1, 1, 1));
+        assertEquals(0, GeoHash.hashLengthToCoverBoundingBox(1, -1, 1, 1));
+        assertEquals(0, GeoHash.hashLengthToCoverBoundingBox(1, 1, -1, 1));
+        assertEquals(0, GeoHash.hashLengthToCoverBoundingBox(1, 1, 1, -1));
+        assertEquals(0, GeoHash.hashLengthToCoverBoundingBox(1, -1, -1, 1));
+        assertEquals(12, GeoHash.hashLengthToCoverBoundingBox(1, -1, 1, -1));
+        assertEquals(0, GeoHash.hashLengthToCoverBoundingBox(1, -1, -1, -1));
+        assertEquals(0, GeoHash.hashLengthToCoverBoundingBox(1, 1, -1, -1));
+
+        assertEquals(12, GeoHash.hashLengthToCoverBoundingBox(-1, -1, -1, -1));
+        assertEquals(0, GeoHash.hashLengthToCoverBoundingBox(-1, 1, -1, -1));
+        assertEquals(0, GeoHash.hashLengthToCoverBoundingBox(-1, -1, 1, -1));
+        assertEquals(0, GeoHash.hashLengthToCoverBoundingBox(-1, -1, -1, 1));
+        assertEquals(0, GeoHash.hashLengthToCoverBoundingBox(-1, 1, 1, -1));
+        assertEquals(12, GeoHash.hashLengthToCoverBoundingBox(-1, 1, -1, 1));
+        assertEquals(0, GeoHash.hashLengthToCoverBoundingBox(-1, 1, 1, 1));
+        assertEquals(0, GeoHash.hashLengthToCoverBoundingBox(-1, -1, 1, 1));
+    }
+
+    @Test
+    public void widthDegreesWithNBiggerThanMAX_HASH_LENGTH() {
+        assertEquals(4.190951585769653E-8, GeoHash.widthDegrees(13), 0.001);
+    }
+
+    @Test
+    public void widthDegreesWithNSmallerThanMAX_HASH_LENGTH() {
+        assertEquals(1.341104507446289E-6, GeoHash.widthDegrees(11), 0.001);
+    }
+
+    @Test
+    public void heightDegreesWithNBiggerThanMAX_HASH_LENGTH() {
+        assertEquals(4.190951585769653E-8, GeoHash.heightDegrees(13), 0.001);
+    }
+
+    @Test
+    public void heightDegreesWithNSmallerThanMAX_HASH_LENGTH() {
+        assertEquals(1.341104507446289E-6, GeoHash.heightDegrees(11), 0.001);
+    }
+
+    @Test
+    public void coverBoundingBoxLongsWithSixteenBoundary() {
+        assertEquals(CoverageLongs.class, GeoHash.coverBoundingBoxLongs(1, 1, 1, 1, 1).getClass());
+        assertEquals(CoverageLongs.class, GeoHash.coverBoundingBoxLongs(1, -1, 1, 1, 1).getClass());
+        assertEquals(CoverageLongs.class, GeoHash.coverBoundingBoxLongs(1, 1, -1, 1, 1).getClass());
+        assertEquals(CoverageLongs.class, GeoHash.coverBoundingBoxLongs(1, -1, -1, 1, 1).getClass());
+        assertEquals(CoverageLongs.class, GeoHash.coverBoundingBoxLongs(1, -1, 1, -1, 1).getClass());
+        assertEquals(CoverageLongs.class, GeoHash.coverBoundingBoxLongs(1, -1, -1, -1, 1).getClass());
+        assertEquals(CoverageLongs.class, GeoHash.coverBoundingBoxLongs(-1, -1, -1, -1, 1).getClass());
+        assertEquals(CoverageLongs.class,GeoHash.coverBoundingBoxLongs(-1, -1, -1, 1, 1).getClass());
+        assertEquals(CoverageLongs.class,GeoHash.coverBoundingBoxLongs(-1, 1, -1, 1, 1).getClass());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void coverBoundingBoxLongsWithException1() {
+        assertEquals(CoverageLongs.class, GeoHash.coverBoundingBoxLongs(1, 1, 1, -1, 1).getClass());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void coverBoundingBoxLongsWithException2() {
+        assertEquals(CoverageLongs.class, GeoHash.coverBoundingBoxLongs(1, 1, -1, -1, 1).getClass());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void coverBoundingBoxLongsWithException3() {
+        assertEquals(CoverageLongs.class, GeoHash.coverBoundingBoxLongs(-1, 1, -1, -1, 1).getClass());
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void coverBoundingBoxLongsWithException4() {
+        assertEquals(CoverageLongs.class,GeoHash.coverBoundingBoxLongs(-1, 1, 1, -1, 1).getClass());
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void coverBoundingBoxLongsWithException5() {
+        assertEquals(CoverageLongs.class,GeoHash.coverBoundingBoxLongs(-1, 1, 1, 1, 1).getClass());
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void coverBoundingBoxLongsWithException6() {
+        assertEquals(CoverageLongs.class,GeoHash.coverBoundingBoxLongs(-1, -1, 1, 1, 1).getClass());
+        assertEquals(CoverageLongs.class,GeoHash.coverBoundingBoxLongs(-1, -1, 1, -1, 1).getClass());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void coverBoundingBoxLongsWithException7() {
+        assertEquals(CoverageLongs.class,GeoHash.coverBoundingBoxLongs(-1, -1, 1, -1, 1).getClass());
+    }
+
+    @Test
+    public void coverBoundingBoxMaxHashesWithoutExceptionAndEnoughMaxHashes() {
+        assertEquals(Coverage.class,GeoHash.coverBoundingBoxMaxHashes(1, 1, 1, 1, 2147483647).getClass());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void coverBoundingBoxMaxHashesWithExceptionAndEnoughMaxHashes() {
+        assertEquals(Coverage.class,GeoHash.coverBoundingBoxMaxHashes(1, 1, 1, -1, 2147483647).getClass());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void coverBoundingBoxMaxHashesWithoutExceptionAndNotEnoughMaxHashes() {
+        assertEquals(Coverage.class,GeoHash.coverBoundingBoxMaxHashes(1, 1, 1, 1, -2147483648).getClass());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void coverBoundingBoxMaxHashesWithExceptionAndNotEnoughMaxHashes() {
+        assertEquals(Coverage.class,GeoHash.coverBoundingBoxMaxHashes(1, 1, 1, -1, -2147483648).getClass());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void coverBoundingBoxMaxHashesWithExceptionAndNotEnoughMaxHashes2() {
+        assertEquals(Coverage.class,GeoHash.coverBoundingBoxMaxHashes(-1, 1, 1, -1, -2147483648).getClass());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void coverBoundingBoxMaxHashesWithExceptionAndNotEnoughMaxHashes3() {
+        assertEquals(Coverage.class,GeoHash.coverBoundingBoxMaxHashes(-1, 1, 1, 1, -2147483648).getClass());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void coverBoundingBoxMaxHashesWithExceptionAndEnoughMaxHashes2() {
+        assertEquals(Coverage.class,GeoHash.coverBoundingBoxMaxHashes(-1, 1, 1, -1, 2147483647).getClass());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void coverBoundingBoxMaxHashesWithExceptionAndEnoughMaxHashes3() {
+        assertEquals(Coverage.class,GeoHash.coverBoundingBoxMaxHashes(-1, 1, 1, 1, 2147483647).getClass());
+    }
+
+    @Test
+    public void right() {
+        assertEquals("28",GeoHash.right("22"));
+        assertEquals("b",GeoHash.right("i"));
+        assertEquals("c",GeoHash.right("b"));
+        assertEquals("f1",GeoHash.right("cc"));
+    }
+
+    @Test
+    public void left() {
+        assertEquals("20",GeoHash.left("22"));
+        assertEquals("r",GeoHash.left("2"));
+        assertEquals("p",GeoHash.left("0"));
+        assertEquals("0c",GeoHash.left("11"));
+    }
+
+    @Test
+    public void top() {
+        assertEquals("ac",GeoHash.top("ab"));
+        assertEquals("u",GeoHash.top("b"));
+        assertEquals("r",GeoHash.top("p"));
+        assertEquals("u0",GeoHash.top("bp"));
+    }
+
+    @Test
+    public void bottom() {
+        assertEquals("10",GeoHash.bottom("11"));
+        assertEquals("1",GeoHash.bottom("3"));
+        assertEquals("h",GeoHash.bottom("0"));
+        assertEquals("hp",GeoHash.bottom("00"));
     }
 }
